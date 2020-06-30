a79939a7d79ec55f3706a51ba45f35528c5f9150
pyun <pyun@ust.hk>
2020-06-30 10:29:58 +0800

    tune finetune-l2sp

--- run_scripts/dev-tune-finetune-l2sp/run-tune-finetune-l2sp.sh
+++ run_scripts/dev-tune-finetune-l2sp/run-tune-finetune-l2sp.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
+
 CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-l2sp-1.0 \
-    --cfg-path configs/dev/fine_tuning_l2sp_1.py \
+    --tag incdet-fine-tuning-l2sp-0.2 \
+    --cfg-path configs/dev-tune-finetune-l2sp/fine_tuning_l2sp_0.2.py \
+    --mode train &
+
+CUDA_VISIBLE_DEVICES="2" python3 main.py \
+    --tag incdet-fine-tuning-l2sp-0.1 \
+    --cfg-path configs/dev-tune-finetune-l2sp/fine_tuning_l2sp_0.1.py \
+    --mode train &
+
+CUDA_VISIBLE_DEVICES="3" python3 main.py \
+    --tag incdet-fine-tuning-l2sp-0.05 \
+    --cfg-path configs/dev-tune-finetune-l2sp/fine_tuning_l2sp_0.05.py \
     --mode train
 
 CUDA_VISIBLE_DEVICES="0" python3 main.py \
     --tag incdet-fine-tuning-l2sp-0.5 \
-    --cfg-path configs/dev/fine_tuning_l2sp_0.5.py \
-    --mode train
+    --cfg-path configs/dev-tune-finetune-l2sp/fine_tuning_l2sp_0.5.py \
+    --mode train &
 
-CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-l2sp-0.2 \
-    --cfg-path configs/dev/fine_tuning_l2sp_0.2.py \
-    --mode train
-
-CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-l2sp-0.1 \
-    --cfg-path configs/dev/fine_tuning_l2sp_0.1.py \
-    --mode train
-
-CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-l2sp-0.05 \
-    --cfg-path configs/dev/fine_tuning_l2sp_0.05.py \
-    --mode train
-
-CUDA_VISIBLE_DEVICES="0" python3 main.py \
+CUDA_VISIBLE_DEVICES="2" python3 main.py \
     --tag incdet-fine-tuning-l2sp-0.02 \
-    --cfg-path configs/dev/fine_tuning_l2sp_0.02.py \
+    --cfg-path configs/dev-tune-finetune-l2sp/fine_tuning_l2sp_0.02.py \
+    --mode train &
+
+CUDA_VISIBLE_DEVICES="3" python3 main.py \
+    --tag incdet-fine-tuning-l2sp-0.01 \
+    --cfg-path configs/dev-tune-finetune-l2sp/fine_tuning_l2sp_0.01.py \
     --mode train
 
 CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-l2sp-0.01 \
-    --cfg-path configs/dev/fine_tuning_l2sp_0.01.py \
+    --tag incdet-fine-tuning-l2sp-1.0 \
+    --cfg-path configs/dev-tune-finetune-l2sp/fine_tuning_l2sp_1.py \
     --mode train
+
+
