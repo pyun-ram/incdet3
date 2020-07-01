e0649468a088d612da160a0c3d278375c971d094
pyun <pyun@connect.ust.hk>
2020-07-01 19:01:16 +0800

    tune finetuning_delta

--- run_scripts/dev-tune-finetune-delta/run-tune-finetune-delta.sh
+++ run_scripts/dev-tune-finetune-delta/run-tune-finetune-delta.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-delta-1.0-block03bn2 \
-    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_1.0_block03bn2.py \
+    --tag incdet-fine-tuning-delta-0.1-block03bn2 \
+    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_0.1_block03bn2.py \
     --mode train
 
 CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-delta-1.0-block03conv2 \
-    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_1.0_block03conv2.py \
+    --tag incdet-fine-tuning-delta-0.1-block03conv2 \
+    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_0.1_block03conv2.py \
     --mode train
 
 CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-delta-1.0-block04bn2 \
-    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_1.0_block04bn2.py \
+    --tag incdet-fine-tuning-delta-0.1-block04bn2 \
+    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_0.1_block04bn2.py \
     --mode train
 
 CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-delta-1.0-block04conv2 \
-    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_1.0_block04conv2.py \
+    --tag incdet-fine-tuning-delta-0.1-block04conv2 \
+    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_0.1_block04conv2.py \
     --mode train
 
 CUDA_VISIBLE_DEVICES="0" python3 main.py \
-    --tag incdet-fine-tuning-delta-1.0-deblocks02 \
-    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_1.0_deblocks02.py \
+    --tag incdet-fine-tuning-delta-0.1-deblocks02 \
+    --cfg-path configs/dev-tune-finetune-delta/fine_tuning_delta_0.1_deblocks02.py \
     --mode train
