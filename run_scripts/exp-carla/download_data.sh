cb5bf1c8ac229fe69322acdf74974f006399955c
pyun <pyun@connect.ust.hk>
2020-07-07 13:42:59 +0800

    setup exp-carla

--- run_scripts/exp-carla/download_data.sh
+++ run_scripts/exp-carla/download_data.sh
@@ -3,12 +3,12 @@ apt-get update && apt-get install -y unzip
 mkdir /usr/app/data
 cd /usr/app/data
 wget --no-check-certificate \
-     --no-proxy 'https://pyun-data.s3.amazonaws.com/IncDet3/Data/CARLA_SMALL.zip' \
-     --output-document=/usr/app/data/CARLA_SMALL.zip
-unzip CARLA_SMALL.zip
+     --no-proxy 'https://pyun-data.s3.amazonaws.com/IncDet3/Data/CARLA.zip' \
+     --output-document=/usr/app/data/CARLA.zip
+unzip CARLA.zip
 
 cd /usr/app/incdet3/saved_weights/
 wget --no-check-certificate \
-     --no-proxy 'https://pyun-data.s3.amazonaws.com/IncDet3/Weights/incdet-saveweights-July04-expcarlasmall-car.zip' \
+     --no-proxy 'https://pyun-data.s3.amazonaws.com/IncDet3/Weights/incdet-saveweights-July07-expcarla-car.zip' \
      --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
 unzip incdet-saveweights.zip
