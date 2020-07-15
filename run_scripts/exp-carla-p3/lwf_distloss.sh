f97e96f4f6bceefc0a7a92f6b03e7a360b7dc06f
pyun <pyun@connect.ust.hk>
2020-07-15 13:13:22 +0800

    prepare configs and scripts for exp-carla-p2

---
+++ run_scripts/exp-carla-p3/lwf_distloss.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+GPUID="0"
+CUDA_VISIBLE_DEVICES=$GPUID python3 main.py \
+    --tag July14-expcarlap1-lwf-cv0 \
+    --cfg-path configs/exp-carla-p1/lwf_distloss_cv0.py \
+    --mode train
+
+CUDA_VISIBLE_DEVICES=$GPUID python3 main.py \
+    --tag July14-expcarlap1-lwf-cv1 \
+    --cfg-path configs/exp-carla-p1/lwf_distloss_cv1.py \
+    --mode train
+
+CUDA_VISIBLE_DEVICES=$GPUID python3 main.py \
+    --tag July14-expcarlap1-lwf-cv2 \
+    --cfg-path configs/exp-carla-p1/lwf_distloss_cv2.py \
+    --mode train
+
+CUDA_VISIBLE_DEVICES=$GPUID python3 main.py \
+    --tag July14-expcarlap1-lwf-cv3 \
+    --cfg-path configs/exp-carla-p1/lwf_distloss_cv3.py \
+    --mode train
+
+CUDA_VISIBLE_DEVICES=$GPUID python3 main.py \
+    --tag July14-expcarlap1-lwf-cv4 \
+    --cfg-path configs/exp-carla-p1/lwf_distloss_cv4.py \
+    --mode train
+
+for i in {0..4}
+do
+cp saved_weights/July14-expcarlap1-lwf-cv$i/IncDetMain-20000.ckpt \
+   saved_weights/incdet-saveweights/IncDetExpCARLAP1-lwf-cv$i-20000.tckpt
+done
