d5fa77efa9ef5be7e6f853ca7eb5df8f677b4a36
pyun <pyun@connect.ust.hk>
2020-07-18 14:01:37 +0800

    rm tmprun.sh

--- run_scripts/exp-carlamore-eval/tmprun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
- python3 tools/compute_mAP.py \
-    --log-dir logs/July14-expcarlamore/${1}${2} \
-    --val-pkl-path /usr/app/data/CARLA-TOWN01CARPEDCYC/CARLA_infos_val${2}.pkl \
-    --valid-range -35.2 -40 -1.5 35.2 40 2.6 \
-    --valid-classes Car,Pedestrian,Cyclist
