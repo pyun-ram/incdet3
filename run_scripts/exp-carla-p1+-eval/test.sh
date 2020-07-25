b0247ac5b8dc478491240d966f203fd5268cf524
pyun <pyun@connect.ust.hk>
2020-07-25 15:44:25 +0800

    add exp-carla-p1+-eval configs

--- run_scripts/exp-carla-p1+-eval/test.sh
+++ run_scripts/exp-carla-p1+-eval/test.sh
@@ -15,3 +15,21 @@ done
 done
 done
 done
+
+GPUID="0"
+task=task2
+
+for cv in {0..4} ; do
+for reuse_tag in "reuse" "noreuse" ; do
+for domain in "domain1" "domain2" ; do
+for mode in "finetuning" "jointtraining" "lwf" ; do
+./run_scripts/clear_gpus.sh
+CUDA_VISIBLE_DEVICES=$GPUID python3 main.py \
+    --tag July24-expcarlap1+-eval-${mode}-${task}-${domain}-${reuse_tag}-${cv} \
+    --cfg-path configs/exp-carla-p1+-eval/${mode}_${task}_${domain}_${reuse_tag}_${cv}.py \
+    --mode test
+done
+done
+done
+done
+done
