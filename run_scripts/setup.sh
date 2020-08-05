b9a0385809bd491ee86b40c3c8731ed1d204e9a8
pyun <pyun@connect.ust.hk>
2020-08-05 13:57:24 +0800

    update setup.sh to support apex installation

--- run_scripts/setup.sh
+++ run_scripts/setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
 conda install -c conda-forge fire
 pip install nuscenes-devkit
 apt-get update && apt-get install -y libglib2.0-0 libsm6 libxrender1 libfontconfig1
 cd /usr/app
 git clone https://git.ram-lab.com/yun/nuscenes-devkit-pyun
-echo "You need to manually add /usr/app/nuscenes-devkit-pyun/python-sdk to PYTHONPATH"
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+echo "${YELLOW}You need to manually add /usr/app/nuscenes-devkit-pyun/python-sdk to PYTHONPATH${NC}"
