4ad7612d83aba393610f12d08e313275b4a7e5b8
pyun <pyun@connect.ust.hk>
2020-08-13 19:32:45 +0800

    add exp-kitti2+3 training config files and running scripts

--- run_scripts/exp-kitti-2+3/finetuning.sh
+++ run_scripts/exp-kitti-2+3/finetuning.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200813-expkitti2+3-train_class2 \
-    --cfg-path configs/exp-kitti-2+3/train_class2.py \
+    --tag 20200813-expkitti2+3-finetuning \
+    --cfg-path configs/exp-kitti-2+3/finetuning.py \
     --mode train
