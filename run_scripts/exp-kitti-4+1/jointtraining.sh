b70b9124718e220b819d4b9a1bcfb513dc540b26
pyun <pyun@connect.ust.hk>
2020-08-18 11:17:17 +0800

    modify the tag of run_scripts/exp-kitti-4+1/jointtraining.sh

--- run_scripts/exp-kitti-4+1/jointtraining.sh
+++ run_scripts/exp-kitti-4+1/jointtraining.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200812-expkitti4+1-jointtraining \
+    --tag 20200818-expkitti4+1-jointtraining \
     --cfg-path configs/exp-kitti-4+1/jointtraining.py \
     --mode train
