0fb67d91dd876c5dd53835706606b369b5e43b86
pyun <pyun@connect.ust.hk>
2020-08-18 11:20:21 +0800

    modify the tag of run_scripts/exp-kitti-4+1/eval_*.sh

--- run_scripts/exp-kitti-4+1/eval_on_all_tasks.sh
+++ run_scripts/exp-kitti-4+1/eval_on_all_tasks.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-#     --tag 20200813-expkitti4+1-A1to4all-eval \
+#     --tag 20200818-expkitti4+1-A1to4all-eval \
 #     --cfg-path configs/exp-kitti-4+1/A1to4all-eval.py \
 #     --mode test
 
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200813-expkitti4+1-B5lwfall-eval \
+    --tag 20200818-expkitti4+1-B5lwfall-eval \
     --cfg-path configs/exp-kitti-4+1/B5lwfall-eval.py \
     --mode test
 
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200813-expkitti4+1-B5jointtrainingall-eval \
+    --tag 20200818-expkitti4+1-B5jointtrainingall-eval \
     --cfg-path configs/exp-kitti-4+1/B5jointtrainingall-eval.py \
     --mode test
 
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200813-expkitti4+1-B5finetuningall-eval \
+    --tag 20200818-expkitti4+1-B5finetuningall-eval \
     --cfg-path configs/exp-kitti-4+1/B5finetuningall-eval.py \
     --mode test
 
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200813-expkitti4+1-A1to5all-eval \
+    --tag 20200818-expkitti4+1-A1to5all-eval \
     --cfg-path configs/exp-kitti-4+1/A1to5all-eval.py \
     --mode test
--- run_scripts/exp-kitti-4+1/eval_on_old_tasks.sh
+++ run_scripts/exp-kitti-4+1/eval_on_old_tasks.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-#     --tag 20200813-expkitti4+1-A1to4old-eval \
+#     --tag 20200818-expkitti4+1-A1to4old-eval \
 #     --cfg-path configs/exp-kitti-4+1/A1to4old-eval.py \
 #     --mode test
 
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200813-expkitti4+1-B5lwfold-eval \
+    --tag 20200818-expkitti4+1-B5lwfold-eval \
     --cfg-path configs/exp-kitti-4+1/B5lwfold-eval.py \
     --mode test
 
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200813-expkitti4+1-B5jointtrainingold-eval \
+    --tag 20200818-expkitti4+1-B5jointtrainingold-eval \
     --cfg-path configs/exp-kitti-4+1/B5jointtrainingold-eval.py \
     --mode test
 
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200813-expkitti4+1-B5finetuningold-eval \
+    --tag 20200818-expkitti4+1-B5finetuningold-eval \
     --cfg-path configs/exp-kitti-4+1/B5finetuningold-eval.py \
     --mode test
 
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200813-expkitti4+1-A1to5old-eval \
+    --tag 20200818-expkitti4+1-A1to5old-eval \
     --cfg-path configs/exp-kitti-4+1/A1to5old-eval.py \
     --mode test
