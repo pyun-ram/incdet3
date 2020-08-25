18a8570de791e3b10c753415e4cc2fe075d65190
pyun <pyun@connect.ust.hk>
2020-08-25 15:06:43 +0800

    Add exp-kitti-4+1 lwf (unbiased sampling & threshold sampling) config files & running scripts

--- run_scripts/exp-kitti-4+1/lwf.sh
+++ run_scripts/exp-kitti-4+1/lwf.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+# CUDA_VISIBLE_DEVICES=$1 python3 main.py \
+#     --tag 20200818-expkitti4+1-lwf \
+#     --cfg-path configs/exp-kitti-4+1/lwf.py \
+#     --mode train
+
 CUDA_VISIBLE_DEVICES=$1 python3 main.py \
-    --tag 20200818-expkitti4+1-lwf \
-    --cfg-path configs/exp-kitti-4+1/lwf.py \
+    --tag 20200825-expkitti4+1-lwf_unbiased \
+    --cfg-path configs/exp-kitti-4+1/lwf_unbiased.py \
+    --mode train
+
+CUDA_VISIBLE_DEVICES=$1 python3 main.py \
+    --tag 20200825-expkitti4+1-lwf_threshold \
+    --cfg-path configs/exp-kitti-4+1/lwf_threshold.py \
     --mode train
