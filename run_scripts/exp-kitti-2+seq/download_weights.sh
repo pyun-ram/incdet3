946fb8504bf6dd31ae640f6635167f88e4657317
pyun <pyun@connect.ust.hk>
2020-09-08 13:19:20 +0800

    Update config files and running scripts to support exp-kitti-2+seq/(kd)ewc3_ewc.py

--- run_scripts/exp-kitti-2+seq/download_weights.sh
+++ run_scripts/exp-kitti-2+seq/download_weights.sh
@@ -11,3 +11,8 @@ wget --no-check-certificate \
      --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200905-ewcweights-compute_clsterm_regterm.zip' \
      --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
 unzip incdet-saveweights.zip
+
+wget --no-check-certificate \
+     --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200907-expkitti2%2Bseq-weights.zip' \
+     --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
+unzip incdet-saveweights.zip
