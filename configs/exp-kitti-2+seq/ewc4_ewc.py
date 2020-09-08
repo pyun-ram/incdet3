e855dfcd9645749ef953e1038bfe840778382199
pyun <pyun@connect.ust.hk>
2020-09-08 10:40:33 +0800

    1. Update config files for kitt-ewc expeirments * set the TBDewc_coefTBD as 0.1*160*132 2. Update download_weights of exp-kitti-4+1

--- configs/exp-kitti-2+seq/ewc4_ewc.py
+++ configs/exp-kitti-2+seq/ewc4_ewc.py
@@ -184,7 +184,7 @@ cfg.NETWORK = {
     "@delta_coef": 0.01,
     "@distillation_loss_cls_coef": 0.1,
     "@distillation_loss_reg_coef": 0.2,
-    "@ewc_coef": TBDewc_coefTBD,
+    "@ewc_coef": 160*132*0.1,
     "@ewc_weights_path": TBDewc_weights_pathTBD,
     "@num_biased_select": 32,
     "@threshold_delta_fgmask": 0.5,
--- run_scripts/exp-kitti-4+1/download_weights.sh
+++ run_scripts/exp-kitti-4+1/download_weights.sh
@@ -6,3 +6,9 @@ wget --no-check-certificate \
      --no-proxy 'https://pyun-data.s3.amazonaws.com/IncDet3/Weights/20200812-expkitti4%2B1-saved_weights.zip' \
      --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
 unzip incdet-saveweights.zip
+
+cd /usr/app/incdet3/saved_weights/
+wget --no-check-certificate \
+     --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200907-ewcweights-train_class4_ewc.zip' \
+     --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
+unzip incdet-saveweights.zip
