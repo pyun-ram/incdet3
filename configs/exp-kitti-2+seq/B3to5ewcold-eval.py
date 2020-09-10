89ab30150df0b45dd2f4aa2e6564612cda154488
pyun <pyun@connect.ust.hk>
2020-09-10 20:09:50 +0800

    Update config files for evaluation exp-kitti-2+seq-(kd)ewc

--- configs/exp-kitti-2+seq/B3to5ewcold-eval.py
+++ configs/exp-kitti-2+seq/B3to5ewcold-eval.py
@@ -135,7 +135,7 @@ cfg.NETWORK = {
     "@classes_target": ["Car", "Pedestrian", "Cyclist", "Van", "Truck"],
     "@classes_source": None,
     "@model_resume_dict": {
-        "ckpt_path": TBDckpt_pathTBD,
+        "ckpt_path": "saved_weights/20200907-expkitti2+seq-ewc-saved_weights/20200907-expkitti2+seq-ewc5/IncDetMain-34704.tckpt",
         "num_classes": 2,
         "num_anchor_per_loc": 4,
         "partially_load_params": [],
