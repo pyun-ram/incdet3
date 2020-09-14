e73c72c0c2c733c4ae17c214f692313e4e1858d3
pyun <pyun@connect.ust.hk>
2020-09-14 13:53:30 +0800

    Update exp-kitti-2+seq/download_weights.sh

--- run_scripts/exp-kitti-2+seq/download_weights.sh
+++ run_scripts/exp-kitti-2+seq/download_weights.sh
@@ -9,12 +9,12 @@ unzip incdet-saveweights.zip
 
 cd /usr/app/incdet3/saved_weights/
 wget --no-check-certificate \
-     --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200912-ewcweights-compute_terms_v2+.zip' \
+     --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200912-ewcweights-compute_terms_v2%2B.zip' \
      --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
 unzip incdet-saveweights.zip
 
 cd /usr/app/incdet3/saved_weights/
 wget --no-check-certificate \
-     --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200913-expkitti2+seq-weights.zip' \
+     --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200913-expkitti2%2Bseq-weights.zip' \
      --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
 unzip incdet-saveweights.zip
