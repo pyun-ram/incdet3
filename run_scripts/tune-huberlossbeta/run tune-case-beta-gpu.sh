6253d45cb9dd80cd53970220e6c6fd90ff40a99a
pyun <pyun@connect.ust.hk>
2020-09-24 11:48:14 +0800

    prepare config files and running scripts for tuning beta

--- run_scripts/tune-huberlossbeta/run tune-case-beta-gpu.sh
+++ run_scripts/tune-huberlossbeta/run tune-case-beta-gpu.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 case=$1
 beta=$2
-CUDA_VISIBLE_DEVICES=$1 python3 main.py \
+gpu=$3
+CUDA_VISIBLE_DEVICES=${gpu} python3 main.py \
     --tag 20200924-tune-kitti2+3-${case}-huberlossbeta${beta}-cv0 \
     --cfg-path configs/tune-huberlossbeta/${case}-${beta}-cv0.py \
     --mode train
 
-CUDA_VISIBLE_DEVICES=$1 python3 main.py \
+CUDA_VISIBLE_DEVICES=${gpu} python3 main.py \
     --tag 20200924-tune-kitti2+3-${case}-huberlossbeta${beta}-cv1 \
     --cfg-path configs/tune-huberlossbeta/${case}-${beta}-cv1.py \
     --mode train
 
-CUDA_VISIBLE_DEVICES=$1 python3 main.py \
+CUDA_VISIBLE_DEVICES=${gpu} python3 main.py \
     --tag 20200924-tune-kitti2+3-${case}-huberlossbeta${beta}-cv2 \
     --cfg-path configs/tune-huberlossbeta/${case}-${beta}-cv2.py \
     --mode train
