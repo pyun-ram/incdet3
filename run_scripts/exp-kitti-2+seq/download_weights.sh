b7e223e8c353a581b0a3d7967894366c077dd89c
pyun <pyun@connect.ust.hk>
2020-10-08 12:24:27 +0800

    update exp-kitti-2+seq/download_weights.sh

--- run_scripts/exp-kitti-2+seq/download_weights.sh
+++ run_scripts/exp-kitti-2+seq/download_weights.sh
@@ -9,12 +9,12 @@ unzip incdet-saveweights.zip
 
 cd /usr/app/incdet3/saved_weights/
 wget --no-check-certificate \
-     --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200919-ewcweights-kitti2%2Bseq-compute_terms.zip' \
+     --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20201007-masweights-class2.zip' \
      --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
 unzip incdet-saveweights.zip
 
-cd /usr/app/incdet3/saved_weights/
-wget --no-check-certificate \
-     --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200925-expkitti2%2Bseq-weights.zip' \
-     --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
-unzip incdet-saveweights.zip
+# cd /usr/app/incdet3/saved_weights/
+# wget --no-check-certificate \
+#      --no-proxy 'https://pyun-data-hk.s3.ap-east-1.amazonaws.com/IncDet3/Weights/20200925-expkitti2%2Bseq-weights.zip' \
+#      --output-document=/usr/app/incdet3/saved_weights/incdet-saveweights.zip
+# unzip incdet-saveweights.zip
