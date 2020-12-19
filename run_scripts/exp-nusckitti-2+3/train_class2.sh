2d0bdfa2a4109b5f00587a8fe2152d07250adb19
pyun <pyun@connect.ust.hk>
2020-12-19 17:21:36 +0800

    add config file & scripts for exp-nusckitti-2+3 train_class2

---
+++ run_scripts/exp-nusckitti-2+3/train_class2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=$1 python3 main.py \
+    --tag 20201219-expnusckitti2+3-train_class2 \
+    --cfg-path configs/exp-nusckitti-2+3/train_class2.py \
+    --mode train
